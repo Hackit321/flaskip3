4842385bd333064e485f1b880fdbeea3bc3f9010
Hackit321 <kirwamarkk@gmail.com>
2021-09-28 02:19:50 +0300

    creaed the first route endpoint also imported render_templated flask login and other necessary imports

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,14 @@
-from flask import app, render_template
+from app.models import Pitch
+from flask import render_template, redirect, url_for,abort,request
+from . import main
+from flask_login import login_required,current_user
 
 
-@app.route('/')
+
+@main.route('/')
 def index():
-    print('hello there')
+    pitches = Pitch.query.all()
+    job = Pitch.query.filter_by(category = 'Job').all() 
+    event = Pitch.query.filter_by(category = 'Events').all()
+    advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
+    return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
