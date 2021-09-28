b8e23fee851ffb89ac2fa05a57d5f1d746c26545
Hackit321 <kirwamarkk@gmail.com>
2021-09-28 08:26:25 +0300

    created a base html

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db,login_manager
 from datetime import datetime
 from flask_login import UserMixin,current_user
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 
