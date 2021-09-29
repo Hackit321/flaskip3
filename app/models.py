6a357c66b0ad32c4e99bb8ea7b9e0d315c8e51c8
Hackit321 <kirwamarkk@gmail.com>
2021-09-29 11:55:54 +0300

    created the requirement file

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db,login_manager
 from datetime import datetime
 from flask_login import UserMixin,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
+from .models import User
 
 
 
